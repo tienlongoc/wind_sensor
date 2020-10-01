ce7d294f821b87b6727e285505ea0b319daa2584
oscar raspberry <oscar raspberry>
2020-10-01 17:13:38 +0100

    more tweak

--- rasp/post_output_log.sh
+++ rasp/post_output_log.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 source ~/.bashrc
-
+date
 cd /home/pi/ws/wind_sensor/pyserial/logs/
 for log in $(ls *txt); do
-    if -d "api-key=dev" curl --data-binary "@$log" 35.246.96.248:5100; then
-        mv $log $log.posted
-    fi
+    curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev"
 done
