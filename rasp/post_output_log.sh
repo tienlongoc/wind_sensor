d907bc077e2bc8e66ef8f33f5c83c6a3db26d0e9
oscar raspberry <oscar raspberry>
2020-10-02 00:14:29 +0100

    tweak sh scripts

--- rasp/post_output_log.sh
+++ rasp/post_output_log.sh
@@ -4,5 +4,7 @@ source ~/.bashrc
 date
 cd /home/pi/ws/wind_sensor/pyserial/logs/
 for log in $(ls *txt); do
-    curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev"
+    if curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev"; then
+        mv $log $log.posted$(date +"%H%M%S")
+    fi
 done
--- rasp/run_output_log.sh
+++ rasp/run_output_log.sh
@@ -5,13 +5,13 @@ source ~/.bashrc
 # Set up crontab
 if ! crontab -l | grep -q run_output_log.sh; then
     crontab -l > curr_cron
-    echo '00 00 * * 0 /home/pi/ws/wind_sensor/rasp/run_output_log.sh >> /home/pi/ws/wind_sensor/rasp/log.txt 2>&1' >> curr_cron
+    echo '00 00 * * 0 /home/pi/ws/wind_sensor/rasp/run_output_log.sh >> /home/pi/ws/wind_sensor/rasp/log.txt' >> curr_cron
     crontab curr_cron
     rm curr_cron
 fi
 if ! crontab -l | grep -q post_output_log.sh; then
     crontab -l > curr_cron
-    echo '* 00 * * 0 /home/pi/ws/wind_sensor/rasp/post_output_log.sh >> /home/pi/ws/wind_sensor/rasp/log/txt 2>&1' >> curr_cron
+    echo '* 00 * * 0 /home/pi/ws/wind_sensor/rasp/post_output_log.sh >> /home/pi/ws/wind_sensor/rasp/log/txt' >> curr_cron
     crontab curr_cron
     rm curr_cron
 fi
