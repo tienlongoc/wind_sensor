fbdf21162dcc2711850cc599b50cda248d2c2732
oscar raspberry <oscar raspberry>
2020-10-01 16:51:13 +0100

    api key on first line on post request

--- rasp/post_output_log.sh
+++ rasp/post_output_log.sh
@@ -4,7 +4,7 @@ source ~/.bashrc
 
 cd /home/pi/ws/wind_sensor/pyserial/logs/
 for log in $(ls *txt); do
-    if curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev"; then
+    if -d "api-key=dev" curl --data-binary "@$log" 35.246.96.248:5100; then
         mv $log $log.posted
     fi
 done
