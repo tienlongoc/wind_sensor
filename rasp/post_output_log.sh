78519d7c37152268c3ad0dda22b103cfddae3db9
oscar raspberry <oscar raspberry>
2020-10-02 10:08:46 +0100

    better post handling on rasp side

--- rasp/post_output_log.sh
+++ rasp/post_output_log.sh
@@ -4,7 +4,9 @@ source ~/.bashrc
 date
 cd /home/pi/ws/wind_sensor/pyserial/logs/
 for log in $(ls *txt); do
-    if curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev"; then
+    curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev" > tmp 2>&1
+    if grep "post request received successfully" tmp; then
         mv $log $log.posted$(date +"%H%M%S")
+	rm tmp
     fi
 done
