f3f877bbeef68149ec69ff8c4ad0d96d1a04c89b
oscar <oscar.tienlong.chung@gmail.com>
2020-10-02 15:04:38 +0000

    tweak scripts

--- gcp/generate_charts.sh
+++ gcp/generate_charts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ~/.bashrc
+source ./wind_sensor/bin/activate
 
 day=$(date +"%Y%m%d")
 hour=$(date +"%Y%m%d-%H")
@@ -8,3 +9,13 @@ hour=$(date +"%Y%m%d-%H")
 python generate_charts.py --day="$day"
 python generate_charts.py --hour="$hour"
 
+if [ -f ./charts/daily/$day.html ] ; then
+    cp ./charts/daily/$day.html ./charts/daily/current.html
+else
+    cp ./charts/daily/default.html ./charts/daily/current.html
+fi
+if [ -f ./charts/hourly/$hour.html ] ; then
+    cp ./charts/hourly/$hour.html ./charts/hourly/current.html
+else
+    cp ./charts/hourly/default.html ./charts/hourly/current.html
+fi
