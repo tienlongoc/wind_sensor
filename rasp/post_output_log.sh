c35e4bcc3aaf516f44679442796a9a5c1e7e0fd4
oscar raspberry <oscar raspberry>
2020-10-09 13:34:13 +0100

    tweak

--- rasp/post_output_log.sh
+++ rasp/post_output_log.sh
@@ -4,6 +4,7 @@ source ~/.bashrc
 date
 cd /home/pi/ws/wind_sensor/pyserial/logs/
 for log in $(ls *txt); do
+    rm tmp
     curl --data-binary "@$log" 35.246.96.248:5100 -d "api-key=dev" > tmp 2>&1
     if grep "post request received successfully" tmp; then
         echo "post success"
